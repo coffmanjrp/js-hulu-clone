efb8cfd2b0d7da0cd1b130cba1fef8b2ff84d1ae
coffmanjrp <coffmanjrp@gmail.com>
2021-07-19 15:30:32 -0700

    Implement switch welcome tabs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const modal = document.querySelector('.modal');
 const loginBtn = document.querySelector('.login-btn');
 const closeBtn = document.querySelector('.close');
-
-loginBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', outsideClick);
+const welcomeTabContent = document.querySelectorAll('.welcome-tab-content');
+const welcomeTabController = document.querySelectorAll(
+  '.welcome-tab-controller'
+);
+const welcomeTabBg = document.querySelector('.welcome-tab-bg');
+const activeBar = document.querySelector('.active-bar');
 
 function openModal() {
   modal.style.display = 'block';
@@ -19,3 +21,41 @@ function outsideClick(e) {
     modal.style.display = 'none';
   }
 }
+
+loginBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+function removeActive() {
+  welcomeTabContent.forEach((item) => item.classList.remove('active'));
+  welcomeTabController.forEach((item) => item.classList.remove('active'));
+}
+
+function selectTab(e) {
+  e.preventDefault();
+  const activeTab = document.getElementById(`${this.id}-content`);
+  const barWidth = e.target.clientWidth;
+  const barOffset = e.target.offsetLeft;
+
+  removeActive();
+
+  this.classList.add('active');
+  activeTab.classList.add('active');
+  activeBar.style.width = `${barWidth}px`;
+  activeBar.style.transform = `translateX(${barOffset}px)`;
+
+  if (window.screen.width <= 768) {
+    welcomeTabBg.style.backgroundImage = `linear-gradient(
+      rgba(0, 0, 0, 0.8) -39.59%,
+      rgba(0, 0, 0, 0) 117.14%
+    ), url('../img/welcome-${this.id}-small.jpg')`;
+  } else {
+    welcomeTabBg.style.backgroundImage = `linear-gradient(
+      90deg,
+      rgba(0, 0, 0, 0.8) -57.5%,
+      rgba(0, 0, 0, 0) 98.72%
+    ), url('../img/welcome-${this.id}.jpg')`;
+  }
+}
+
+welcomeTabController.forEach((tab) => tab.addEventListener('click', selectTab));
